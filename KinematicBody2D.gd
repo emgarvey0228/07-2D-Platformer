381f8e5de1515624e34326b3584c12da809671f6
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-03-03 01:18:59 -0500

    completed a player, movement and jump

---
+++ KinematicBody2D.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+var motion = Vector2()
+export var speed = 200
+export var gravity = 50
+export var jumpForce = -400
+
+func _physics_process(delta):
+	motion.y += gravity
+	if Input.is_action_pressed("ui_right"):
+		motion.x = speed
+	
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+		
+	else:
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = jumpForce
+	
+	motion = move_and_slide(motion, UP)
