42661f967813fc0caf12581988b22d9138083359
Emily Garvey <54558528+emgarvey@users.noreply.github.com>
2020-03-05 23:49:53 -0500

    restart

--- Scenes/Lives.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	pass
-
-func update_lives(l):
-	text = "Lives: " + str(l)
--- Scenes/Score.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-
-func _ready():
-	pass
-
-func update_score(s):
-	text = 'Score: ' + str(s)
